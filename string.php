a181b1e0f05472fda821441886c0b47605fbcf9b
jooyeong <young951109@naver.com>
2018-08-27 13:36:36 +0900

    문자열과 문자열 처리

---
+++ string.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<body>
+  <h1>String & String Operator</h1>
+<?php
+echo "Hello world!";
+echo "Hello \"w\"orld"; //escape - 역할을 하지 못하도록 함.
+ ?>
+ <h2>Concatenation Operator</h2>
+ <?php
+echo "Hello"." world"; //'+'가 아니라 '.'으로 문자열을 더함.
+ ?>
+ <h2>String length function</h2>
+ <?php
+ echo strlen("Hello world"); //문자열의 개수를 알려주는 함수 : strlen 
+ ?>
+</body>
+</html>
