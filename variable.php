37a78e7858990fa1ff1c8b499fbc369833fe67ee
jooyeong <young951109@naver.com>
2018-08-27 13:47:18 +0900

    변수

---
+++ variable.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html>
+<body>
+	<h1>Variable</h1>
+	<?php
+	$name = "leezche";
+	echo "Lorem ipsum dolor ".$name." sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad ".$name." minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit".$name." in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+	?>
+</body>
+</html>
