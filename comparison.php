34d18a4d1c7eeb8b8cc709b7bcd5e352f8c96a7c
jooyeong <young951109@naver.com>
2018-08-28 14:42:17 +0900

    Boolean과 비교연산자

---
+++ comparison.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+</head>
+<body>
+<h1>Comparison Operators &amp; Boolean data type</h1>
+<h2>1 == 1</h2>
+<?php
+var_dump(1 == 1); //var_dump() : 매개변수와 매개변수의 데이터형도 같이 출력해줌.
+?>
+<h2>1>1</h2>
+<?php
+var_dump(1>1);
+?>
+<h2>1>=1</h2>
+<?php
+var_dump(1>=1);
+?>
+</body>
+</html>
