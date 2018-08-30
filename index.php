790fcb2f2d4cf71801b2c1c006c705510ae00328
jooyeong <young951109@naver.com>
2018-08-30 17:16:13 +0900

    반복문과 배열의 활용

--- index.php
+++ index.php
@@ -7,9 +7,32 @@
 <body>
 	<h1><a href="index.php">WEB</a></h1>
 	<ol>
-		<li><a href="index.php?id=HTML">HTML</a></li>
-		<li><a href="index.php?id=CSS">CSS</a></li>
-		<li><a href="index.php?id=JavaScript">JavaScript</a></li>
+		<?php
+			// data directory 안의 파일 읽어오는 방법  : scandir()
+			$list = scandir('./data');
+			// var_dump($list); //형식 알아보기
+
+			$i = 0;
+			while($i < count($list)) {
+				if($list[$i] != '.') {
+					if($list[$i] != '..') {
+						echo "<li><a href=\"index.php?id=$list[$i]\">$list[$i]</a></li>\n";
+					}
+				}
+				$i = $i + 1;
+			}
+
+			/*
+			echo "<li>$list[0]</li>\n";
+			echo "<li>$list[1]</li>\n";
+			echo "<li>$list[2]</li>\n";
+			echo "<li>$list[3]</li>\n";
+			echo "<li>$list[4]</li>\n";
+			echo "<li>$list[5]</li>\n";
+			*/
+
+
+		?>
 	</ol>
 	<h2>
 		<?php
