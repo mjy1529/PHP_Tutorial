05d8c47f58c41c83ce072d2b119a175916c2f043
jooyeong <young951109@naver.com>
2018-09-02 14:25:21 +0900

    글삭제 기능 구현하기

--- index.php
+++ index.php
@@ -58,6 +58,17 @@
 		?>
 	</ol>
 	<a href="create.php">create</a>
+	<?php
+		if(isset($_GET['id'])) { ?>
+			<!--<a href="update.php?id=<?php echo $_GET['id']; ?>">update</a>-->
+			<a href="update.php?id=<?= $_GET['id'] ?>">update</a>
+			<form action="delete_process.php" method="POST">
+				<input type="hidden" name="id" value="<?= $_GET['id'] ?>">
+				<input type="submit" value="delete">
+			</form>
+	<?php
+		}
+	?>	
 	<h2>
 		<?php
 		print_title();
