07c806639925adb94c70028ac02a128a51a62546
jooyeong <young951109@naver.com>
2018-09-03 15:09:49 +0900

    mysqli_error()를 이용해서 디버깅하기

--- PHP_MySQL/insert.php
+++ PHP_MySQL/insert.php
@@ -3,8 +3,7 @@
 	//mysqli_connect() : PHP에서 MySQL을 연결해주는 함수
 	//사용법 : mysqli_connect(아이피, 아이디, 비밀번호, DB명);
 	$conn = mysqli_connect("localhost", "root", "root1111", "opentutorials");
-	
-	mysqli_query($conn, "
+	$sql =  "
 		INSERT INTO topic
 			(title, description, created)
 			VALUES (
@@ -12,5 +11,14 @@
 					'MySQL is ...',
 					NOW()
 				)
-	");
+	";
+
+	//mysqli_query() : MySQL 서버로 쿼리 전송
+	$result = mysqli_query($conn, $sql); //return값 true or false
+
+	if($result == false) {
+		//mysqli_error() : 에러 출력
+		echo mysqli_error($conn);
+	}
+
 ?>
