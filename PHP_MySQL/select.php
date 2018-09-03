3929c03bee96937dff707880e367268323fa133d
jooyeong <young951109@naver.com>
2018-09-03 16:47:48 +0900

    while문을 사용하여 SELECT의 여러 행 가져오기

--- PHP_MySQL/select.php
+++ PHP_MySQL/select.php
@@ -4,9 +4,22 @@
 	//데이터를 1000개만 가져오기
 	//$sql = "SELECT * FROM topic LIMIT 1000";
 
+	//1 row
+	echo "<h1>Single Row</h1>";
 	$sql = "SELECT * FROM topic WHERE id = 13";
 	$result = mysqli_query($conn, $sql);
 	$row = mysqli_fetch_array($result);
-	echo '<h1>'.$row['title'].'</h1>'; //연관배열
+	echo '<h2>'.$row['title'].'</h2>'; //연관배열
 	echo $row['description'];
+
+	//all rows
+	echo "<h1>Multi Rows</h1>";
+	$sql = "SELECT * FROM topic";
+	$result = mysqli_query($conn, $sql);
+
+	//PHP에선 NULL은 false와 같다!!
+	while($row = mysqli_fetch_array($result)) {
+		echo '<h2>'.$row['title'].'</h2>';
+		echo $row['description'];
+	}
 ?>
