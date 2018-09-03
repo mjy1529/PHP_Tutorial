c7af825aa497ecb4bfdbe39f6823d4369fc69ac1
jooyeong <young951109@naver.com>
2018-09-03 14:51:47 +0900

    mysqli_query()를 이용해서 MySQL server에 SQL문을 전송

--- PHP_MySQL/insert.php
+++ PHP_MySQL/insert.php
@@ -1,3 +1,16 @@
 <?php
-	mysqli_connect("localhost", "root", "root1111", "opentutorials");
+	// ** PHP와 MySQL 연결하기 ** //
+	//mysqli_connect() : PHP에서 MySQL을 연결해주는 함수
+	//사용법 : mysqli_connect(아이피, 아이디, 비밀번호, DB명);
+	$conn = mysqli_connect("localhost", "root", "root1111", "opentutorials");
+	
+	mysqli_query($conn, "
+		INSERT INTO topic
+			(title, description, created)
+			VALUES (
+					'MySQL',
+					'MySQL is ...',
+					NOW()
+				)
+	");
 ?>
