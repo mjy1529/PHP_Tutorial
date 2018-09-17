81a2c969dcd953111495af873f14821a9a19c1b2
jooyeong <young951109@naver.com>
2018-09-17 18:22:25 +0900

    출력 공격의 차단_htmlspecialchars()로 해결 : php에서 html 코드를 사용할 수 있게 함

--- PHP_MySQL/index.php
+++ PHP_MySQL/index.php
@@ -6,7 +6,8 @@
 	$list = '';
 	while($row = mysqli_fetch_array($result)) {
 		//<li><a href=\"index.php?id=13\">MySQL</a></li>
-		$list = $list."<li><a href=\"index.php?id={$row['id']}\">{$row['title']}</a></li>";
+		$escaped_title = htmlspecialchars($row['title']);
+		$list = $list."<li><a href=\"index.php?id={$row['id']}\">{$escaped_title}</a></li>";
 	}
 
 	$article = array(
@@ -20,8 +21,7 @@
 		$sql = "SELECT * FROM topic WHERE id={$filtered_id}";
 		$result = mysqli_query($conn, $sql);
 		$row = mysqli_fetch_array($result);
-		$article['title'] = $row['title'];
-		$article['description'] = $row['description'];
+		$article['title'] = htmlspecialchars($row['title']);		$article['description'] = htmlspecialchars($row['description']);
 	}
 	//print_r($article);
 ?>
